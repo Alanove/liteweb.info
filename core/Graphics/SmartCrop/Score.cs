4a95bc90c270c02762824e7e11a721afb5f4f534
nmassaad <nmassaad@sabis.net>
2018-05-16 16:05:33 +0300

    sync

---
+++ core/Graphics/SmartCrop/Score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lw.GraphicUtils.SmartCrop
+{
+    public class Score
+    {
+        public float detail;
+        public float saturation;
+        public float skin;
+        public float total;
+    }
+
+}
